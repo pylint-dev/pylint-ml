ceaabb375c428edbafe8f90c68df4aa8f058fffd
Peter Hamfelt <peter.hamfelt@gmail.com>
2024-02-16 23:07:56 +0100

    Add torch import checker

--- pylint_ml/plugin.py
+++ pylint_ml/plugin.py
@@ -4,6 +4,8 @@ from pylint.lint import PyLinter
 
 from pylint_ml.checkers.numpy.import_numpy import NumpyImportChecker
 from pylint_ml.checkers.pandas.import_pandas import PandasImportChecker
+from pylint_ml.checkers.tensorflow.import_tensorflow import TensorflowImportChecker
+from pylint_ml.checkers.torch.import_torch import TorchImportChecker
 
 
 def register(linter: PyLinter) -> None:
@@ -13,3 +15,12 @@ def register(linter: PyLinter) -> None:
 
     # Pandas
     linter.register_checker(PandasImportChecker(linter))
+
+    # Tensorflow
+    linter.register_checker(TensorflowImportChecker(linter))
+
+    # Torch
+    linter.register_checker(TorchImportChecker(linter))
+
+    # Sklearn
+    # Scipy
