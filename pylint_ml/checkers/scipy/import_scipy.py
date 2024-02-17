4957dd91da48f4088afd9beb14460a89b2542f89
Peter Hamfelt <peter.hamfelt@gmail.com>
2024-02-18 00:02:41 +0100

    Add scipy import checker

--- pylint_ml/checkers/scipy/import_scipy.py
+++ pylint_ml/checkers/scipy/import_scipy.py
@@ -17,7 +17,7 @@ class ScipyImportChecker(BaseChecker):
     msgs = {
         "W8401": (
             "Direct or aliased Scipy import detected",
-            "scipy-direct-aliased-import",
+            "scipy-import",
             "Using `import scipy` or `import scipy as ...` is not recommended. For better clarity and consistency, "
             "it is advisable to import specific submodules directly, using the `from scipy import ...` syntax. "
             "This approach prevents confusion and aligns with common practices by explicitly stating which "
