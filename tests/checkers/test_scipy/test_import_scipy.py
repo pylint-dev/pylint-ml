639dd58a97412d87400fccd047cc1f29b5ee004b
Peter Hamfelt <peter.hamfelt@gmail.com>
2024-02-18 00:01:55 +0100

    Add scipy import checker

---
+++ tests/checkers/test_scipy/test_import_scipy.py
@@ -0,0 +1,36 @@
+import astroid
+import pylint.testutils
+from pylint.interfaces import HIGH
+
+from pylint_ml.checkers.scipy.import_scipy import ScipyImportChecker
+
+
+class TestTorchImport(pylint.testutils.CheckerTestCase):
+    CHECKER_CLASS = ScipyImportChecker
+
+    def test_correct_scipy_import(self):
+        scipy_import_node = astroid.extract_node(
+            """
+        from scipy.misc import imread, imsave, imresize
+        """
+        )
+
+        with self.assertNoMessages():
+            self.checker.visit_import(scipy_import_node)
+
+    def test_incorrect_scipy_import(self):
+        scipy_import_node = astroid.extract_node(
+            """
+        import scipy as spy
+        """
+        )
+
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(
+                msg_id="scipy-import",
+                confidence=HIGH,
+                node=scipy_import_node,
+            ),
+            ignore_position=True,
+        ):
+            self.checker.visit_import(scipy_import_node)
